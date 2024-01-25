2b9c14202e1327de694045d7e4c48e4e8d7f7123
cmenke <cmenke@student.42.fr>
2024-01-25 21:28:20 +0000

    added o protection

--- ex01/src/Serializer.cpp
+++ ex01/src/Serializer.cpp
@@ -1,5 +1,7 @@
 #include "Serializer.hpp"
 
+#include <cstddef>
+
 class Data;
 
 Serializer::Serializer() {}
@@ -22,10 +24,14 @@ Serializer::~Serializer() {}
 
 uintptr_t Serializer::serialize(Data *ptr)
 {
+	if (!ptr)
+		return (0);
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data* Serializer::deserialize(uintptr_t raw)
 {
+	if (!raw)
+		return (NULL);
 	return (reinterpret_cast<Data *>(raw));
 }
